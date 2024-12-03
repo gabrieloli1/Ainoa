fa9a516a280e66fb31682fd6b926c92d26ca44a2
henri <henri@LAPTOP-5H96KJ60>
2024-12-03 00:02:48 -0300

    produto

--- ProjetoBD/paginaProduto.aspx.designer.cs
+++ ProjetoBD/paginaProduto.aspx.designer.cs
@@ -24,6 +24,33 @@ namespace ProjetoBD
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Controle pnlSidenav.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlSidenav;
+
+        /// <summary>
+        /// Controle btnClose.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnClose;
+
+        /// <summary>
+        /// Controle btnImage.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnImage;
+
+        /// <summary>
         /// Controle txtNomeProd.
         /// </summary>
         /// <remarks>
