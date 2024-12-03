54e1819946d9c11dfe9d97d3c26c1d445fdaf6ba
henri <henri@LAPTOP-5H96KJ60>
2024-12-02 23:09:57 -0300

    commiti catalogo

---
+++ ProjetoBD/paginaCatalogo.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjetoBD
+{
+    public partial class paginaPaleta : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlSidenav.CssClass = "sidenav";
+            }
+        }
+
+        protected void btnImage_Click(object sender, EventArgs e)
+        {
+            pnlSidenav.CssClass = "sidenav visible";
+        }
+
+        protected void btnClose_Click(object sender, EventArgs e)
+        {
+            pnlSidenav.CssClass = "sidenav closing";
+        }
+
+    }
+}
